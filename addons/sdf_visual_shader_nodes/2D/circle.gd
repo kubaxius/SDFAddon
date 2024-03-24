66477849fd667900fe45e2c1589fb9cc3d0abc65
Jakub Niedźwiedź <jakub.niedzwiedz98@gmail.com>
2024-03-24 03:34:34 +0100

    Add new shapes, expand RayMarch shader, fix SDRect
    Added shapes:
    - circle
    
    Other changes:
    - Renamed SDRectangle to SDRect, because I think it was shadowing existing Node
    - Added simple way to change shape rendered by RayMarchCube

--- addons/sdf_visual_shader_nodes/2D/circle.gd
+++ addons/sdf_visual_shader_nodes/2D/circle.gd
@@ -1,37 +1,37 @@
-# rect.gd
+# circle.gd
 @tool
 extends VisualShaderNodeCustomSD
-class_name VisualShaderNodeSDRectangle
+class_name VisualShaderNodeSDCircle
 
 
 func _init():
 	## visuals ##
-	name = "SDRect"
+	name = "SDCircle"
 	category = "DistanceFields/2D"
 	icon_type = VisualShaderNode.PORT_TYPE_SCALAR
 	description =\
 """SDF that takes in position in 2D space and returns value based \
-on the distance from a defined rectangle perimeter.
+on the distance from a defined circle perimeter.
 
 Negative value indicates that the point is inside of a shape, \
 and positive indicates that it's outside of it."""
 	
-	function_name = "sdRectNode"
+	function_name = "sdCircleNode"
 	
 	## inputs ##
 	input_names = [\
 	"point",\
-	"dimensions",\
+	"radius",\
 	]
 	
 	input_types = [\
 	VisualShaderNode.PORT_TYPE_VECTOR_2D,\
-	VisualShaderNode.PORT_TYPE_VECTOR_2D,\
+	VisualShaderNode.PORT_TYPE_SCALAR,\
 	]
 	
 	input_default_values = [\
 	Vector2.ZERO,\
-	Vector2.ONE,\
+	1.0,\
 	]
 	
 	## outputs ##
